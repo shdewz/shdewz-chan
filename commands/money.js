8ff980a0a0c65dc7fdc71f954d73d3a07c42ea60
shdewz <shdewzofficial@gmail.com>
2019-12-04 15:57:12 +0200

    Make search case insensitive

--- commands/money.js
+++ commands/money.js
@@ -9,9 +9,9 @@ module.exports = {
         if (args.length >= 1) convertedName = args[0]; // !money <someone else>
         for (var i = 0; i < stat.captains.length; i++) // go through the list
         {
-            if (stat.captains[i].name == convertedName) // see if name exists in captains list
+            if (stat.captains[i].name.toLowerCase() == convertedName.toLowerCase()) // see if name exists in captains list
             {
-                message.channel.send(`\`${convertedName}\` has \`${stat.captains[i].money}\` ${config.currency}`);
+                message.channel.send(`\`${stat.captains[i].name}\` has \`${stat.captains[i].money}\` ${config.currency}`);
                 return;
             }
         }
