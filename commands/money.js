674d74a0b8e712a3304442bca977762b1ab95e34
shdewz <shdewzofficial@gmail.com>
2019-12-04 22:53:14 +0200

    Add check for being a captain

--- commands/money.js
+++ commands/money.js
@@ -5,8 +5,12 @@ module.exports = {
     description: `Displays a captain's money.`,
     execute(message, args, stat)
     {
-        convertedName = message.member.displayName.split(' ').join('_'); // replace spaces with underscores
-        if (args.length >= 1) convertedName = args[0]; // !money <someone else>
+        if (!args && message.member.roles.find(r => r.name === "Captain"))
+        {
+            convertedName = message.member.displayName.split(' ').join('_'); // replace spaces with underscores
+        }
+        else if (args.length >= 1) convertedName = args[0]; // !money <someone else>
+        else return message.reply(`you are not a captain.`);
         for (var i = 0; i < stat.captains.length; i++) // go through the list
         {
             if (stat.captains[i].name.toLowerCase() == convertedName.toLowerCase()) // see if name exists in captains list
