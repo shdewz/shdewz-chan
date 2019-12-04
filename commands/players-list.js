5cfc41de1e748df19b7c3cf6aa200420aba8d78b
shdewz <shdewzofficial@gmail.com>
2019-12-04 22:54:57 +0200

    Add sold display

--- commands/players-list.js
+++ commands/players-list.js
@@ -11,36 +11,39 @@ module.exports = {
             {
                 if (stat.players[i].name.toLowerCase() == args[0].toLowerCase())
                 {
-                    message.channel.send(`**Player name:** \`${stat.players[i].name}\`\n**Pickup line:**    ${stat.players[i].story}`);
+                    message.channel.send(`**Player name:** \`${stat.players[i].name}\`\n**Pickup line:**    ${stat.players[i].story}\s**Sold?** ${stat.players[i].sold}`);
                     return;
                 }
             }
         }
         else
         {
-            var plrListTextNames = "";
-            var plrListTextSold = "";
-
-            for (var i = 0; i < stat.players.length; i++)
+            if (stat.players.length == 0) return message.channel.send(`Player list is probably empty.`);
+            else
             {
-                plrListTextNames += `\`${stat.players[i].name}\`\n`;
-                plrListTextSold += `\`${stat.players[i].sold}\`\n`;
-            }
+                var plrListTextNames = "";
+                var plrListTextSold = "";
 
-            if (plrListTextNames == "")
-            {
-                message.channel.send(`Player list is probably empty.`);
+                for (var i = 0; i < stat.players.length; i++)
+                {
+                    plrListTextNames += `\`${stat.players[i].name}\`\n`;
+                    plrListTextSold += `\`${stat.players[i].sold}\`\n`;
+                }
+
+                if (plrListTextNames == "")
+                {
+                    message.channel.send(`Player list is probably empty.`);
+                    return;
+                }
+
+                const playerListEmbed = new Discord.RichEmbed()
+                    .setColor('#ff007a')
+                    .setTitle(`**Current players** (${stat.players.length})`)
+                    .addField('*Name*', plrListTextNames, true)
+                    .addField('*Sold?*', plrListTextSold, true)
+                message.channel.send(playerListEmbed);
                 return;
             }
-
-            const playerListEmbed = new Discord.RichEmbed()
-                .setColor('#ff007a')
-                .setTitle(`**Current players** (${stat.players.length})`)
-                .addField('*Name*', plrListTextNames, true)
-                .addField('*Sold?*', plrListTextSold, true)
-            message.channel.send(playerListEmbed);
-            return;
         }
-
     }
 };
