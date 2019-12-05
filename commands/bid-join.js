553cbd59b7a6375bfe58b269b82fad6c7b5f8c6d
shdewz <shdewzofficial@gmail.com>
2019-12-05 17:00:43 +0200

    Finish up the process

--- commands/bid-join.js
+++ commands/bid-join.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const config = require("../config.json");
 const Discord = require('discord.js');
 
+var endTimeout;
+
 module.exports = {
     name: 'bid.join',
     description: 'Bids in auction.',
@@ -18,7 +20,8 @@ module.exports = {
                 var found = true;
             }
         }
-        if (!found) return message.reply(`\`${message.member.displayName}\` not recognized as a captain.`);
+        if (!found) return console.log(`${message.member.displayName} not recognized as a captain.`); // return if not a captain
+
         // check if bid is correct
         if (bid > bidder.money) return message.reply(`you only have ${bidder.money} ${config.currency}.`);
         else if (bid > config.maxprice) return message.reply(`you can only bid up to ${config.maxprice} ${config.currency} on one player.`);
@@ -27,15 +30,56 @@ module.exports = {
         else if ((bid % 100) != 0) return message.reply(`your bid must be divisible by 100.`);
 
         // bid works
+
+        clearTimeout(endTimeout); // reset timer if active
+
         price = bid;
-        highest = bidder.name;
-        message.channel.send(`**${message.member.displayName}** bid **${bid}** on **${target}**`);
+        highest = bidder;
+        message.channel.send(`**${message.member.displayName}** bid **${ac(bid)} ${config.currency}** on **${target.name}**`);
 
-        // to-do:
-        // 20 second timer
-        // move player to sold state
+        biddingActive = true; // make sure this is true
 
-        if (bid > 3000) biddingActive = false; // temp disable
+        function countdown()
+        {
+            // set timer for 5 seconds
+            endTimeout = setTimeout(function ()
+            {
+                if (!biddingActive) return; // stop if bid has already ended
+
+                // announce winner
+                message.channel.send(`Auction on \`${target.name}\` was won by ${message.author} for a price of \`${ac(price)} ${config.currency}\`!`);
+
+                // set player as sold
+                var obj = { "name": target.name, "owner": highest.name, "price": price };
+                stat.sold.push(obj);
+
+                target.sold = " X "; // mark player as sold
+
+                // add player to captain's stats
+                var obj = { "name": target.name, "price": price };
+                highest.slaves.push(obj)
+
+                highest.money -= price;
+
+                // save
+                fs.writeFile("stats.json", JSON.stringify(stat), function (err)
+                {
+                    if (err) return console.log("error", err);
+                    return console.log("Save successful.");
+                });
+
+                biddingActive = false;
+                return;
+
+            }, 5000);
+        }
+        if (biddingActive) countdown(); // double check because
         return;
     }
 };
+
+// add comma as thousand separator
+function ac(num)
+{
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
