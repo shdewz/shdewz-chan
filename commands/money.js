d99ce9576eb7ae2757fa5ea1f3d9f35b5af46f35
shdewz <shdewzofficial@gmail.com>
2019-12-05 17:01:44 +0200

    Update role checking

--- commands/money.js
+++ commands/money.js
@@ -5,21 +5,38 @@ module.exports = {
     description: `Displays a captain's money.`,
     execute(message, args, stat)
     {
-        if (!args && message.member.roles.find(r => r.name === "Captain"))
+        if (args.length >= 1) { convertedName = args[0]; return displayMoney(message, args, stat); } // !money <someone else>
+        else
         {
-            convertedName = message.member.displayName.split(' ').join('_'); // replace spaces with underscores
-        }
-        else if (args.length >= 1) convertedName = args[0]; // !money <someone else>
-        else return message.reply(`you are not a captain.`);
-        for (var i = 0; i < stat.captains.length; i++) // go through the list
-        {
-            if (stat.captains[i].name.toLowerCase() == convertedName.toLowerCase()) // see if name exists in captains list
+            for (var i = 0; i < stat.captains.length; i++)
             {
-                message.channel.send(`\`${stat.captains[i].name}\` has \`${stat.captains[i].money}\` ${config.currency}`);
-                return;
+                if (stat.captains[i].name.toLowerCase() == message.member.displayName.toLowerCase())
+                {
+                    convertedName = message.member.displayName.split(' ').join('_'); // replace spaces with underscores
+                    return displayMoney(message, args, stat);
+                }
             }
+            return message.reply(`you are not a captain.`);
         }
-        message.reply(`\`${convertedName}\` not found from captains.`);
-        return;
     }
 };
+
+function displayMoney(message, args, stat)
+{
+    for (var i = 0; i < stat.captains.length; i++) // go through the list
+    {
+        if (stat.captains[i].name.toLowerCase() == convertedName.toLowerCase()) // see if name exists in captains list
+        {
+            message.channel.send(`\`${stat.captains[i].name}\` has \`${ac(stat.captains[i].money)}\` ${config.currency}`);
+            return;
+        }
+    }
+    message.reply(`\`${convertedName}\` not found from captains.`);
+    return;
+}
+
+// add comma as thousand separator
+function ac(num)
+{
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
