bb3bf8fee49b157f1e8ad79475fa5ab83db03500
shdewz <shdewzofficial@gmail.com>
2019-12-26 23:08:35 +0200

    Finish up the process

--- commands/bid-start.js
+++ commands/bid-start.js
@@ -2,56 +2,47 @@ const fs = require("fs");
 const config = require("../config.json");
 const Discord = require('discord.js');
 
-global.price = 0;
-global.maxprice;
-global.minbid;
-global.maxbid;
+module.exports.run = async (client, message, args) =>
+{
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Insufficient permissions.`);
+    if (args.length < 1) return message.reply(`Too few arguments.`);
+    if (biddingActive) return message.reply(`Auction already in progress.`);
 
-global.target; // player to be auctioned
-global.captains = []; // array of joined captains
-global.highest; // highest bid
+    var stat = client.commands.get("loadstats").run(); // load stats
 
-module.exports = {
-    name: 'bid.start',
-    description: 'Starts bidding on a player.',
-    execute(message, args, stat)
+    // check if player is a player
+    for (var i = 0; i < stat.players.length; i++)
     {
-        if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Insufficient permissions.`);
-        if (args.length < 1) return message.reply(`Too few arguments.`);
-        if (biddingActive) return message.reply(`Auction already in progress.`);
-
-        // check if player is a player
-        for (var i = 0; i < stat.players.length; i++)
+        if (stat.players[i].name.toLowerCase() == args[0].toLowerCase())
         {
-            if (stat.players[i].name.toLowerCase() == args[0].toLowerCase())
+            // check if in sold
+            for (var j = 0; j < stat.sold.length; j++)
             {
-                // check if in sold
-                for (var j = 0; j < stat.sold.length; j++)
+                if (stat.sold[j].name == args[i])
                 {
-                    if (stat.sold[j].name == args[i])
-                    {
-                        return message.reply(`Player \`${stat.sold[j].name}\` has already been sold.`);
-                    }
+                    return message.reply(`player \`${stat.sold[j].name}\` has already been sold.`);
                 }
-                return startBid(message, args, stat, stat.players[i]);
             }
+            return startBid(message, args, stat, stat.players[i]);
         }
-        return message.reply(`Player \`${args[0]}\` is not set as a player.`);
     }
+    return message.reply(`player \`${args[0]}\` is not set as a player.`);
 };
 
-function startBid(message, args, stat, player)
+function startBid(message, args, stat, target)
 {
-    captains = [];
-    highest = "";
-    target = player;
-
-    for (var k = 0; k < stat.unsold.length; k++)
+    for (var i = 0; i < stat.unsold.length; i++)
     {
-        if (stat.unsold[k].name = target.name)
+        if (stat.unsold[i].name = target.name)
         {
-            stat.unsold.splice(k, 1);
+            stat.unsold.splice(i, 1);
         }
+    }
+
+    // add captain ids to allowed bidders
+    for (var i = 0; i < stat.captains.length; i++)
+    {
+        if (stat.captains[i].dc) stat.bidders[i] = stat.captains[i].dc;
     }
 
     fs.writeFile("stats.json", JSON.stringify(stat), function (err)
@@ -62,10 +53,122 @@ function startBid(message, args, stat, player)
 
     const bidEmbed = new Discord.RichEmbed()
         .setColor('#ff007a')
-        .setDescription(`**${target.name}** is on sale starting at **100 ${config.currency}**!\nCaptains may start bidding by typing numbers.`)
+        .setDescription(`**${target.name}** is on sale starting at **${config.minbid} ${config.currency}**!\nCaptains may start bidding by typing numbers.`)
     message.channel.send(bidEmbed);
 
-    biddingActive = true;
+    var price = 0; // set initial price to 0
+    biddingActive = true; // set bidding to active
+
+    var bid;
+
+    // set channel topic to show bidding progress
+    message.channel.setTopic(`Currently bidding for ${target.name}`);
+
+    // start collecting bids
+    const collector = new Discord.MessageCollector(message.channel, m => !isNaN(m.content) && stat.bidders.includes(m.author.id) && !m.author.bot && parseInt(m.content) < 2 * config.startmoney);
+
+    // start a timer to end bidding
+    var bidtimer = setTimeout(function ()
+    {
+        collector.stop();
+        message.channel.send(`No one bought ${target.name} 😔`)
+
+        // mark player as yeeted
+        for (var i = 0; i < stat.players.length; i++)
+        {
+            if (stat.players[i].name == target.name)
+            {
+                stat.players[i].sold = " - "
+                break;
+            }
+        }
+
+        fs.writeFile("stats.json", JSON.stringify(stat), function (err)
+        {
+            if (err) return console.log("error", err);
+            return console.log("Save successful.");
+        });
+
+        biddingActive = false;
+
+        return message.channel.setTopic("");
+    }, config.timer)
+
+    collector.on('collect', m =>
+    {
+        bid = parseInt(m.content);
+        var result = stat.captains.filter(obj =>
+        {
+            return obj.dc === m.author.id;
+        })
+        var bidder = result[0]
+
+        // check if bid is correct
+        if (bid > bidder.money) return message.channel.send(`${m.author}, you only have ${bidder.money} ${config.currency}.`);
+        else if (bid > config.maxprice) return message.channel.send(`${m.author}, you can only bid up to ${config.maxprice} ${config.currency} on one player.`);
+        else if (bid < (price + config.minbid)) return message.channel.send(`${m.author}, you must bid at least ${config.minbid} ${config.currency} above the current price.`);
+        else if (bid > (price + config.maxbid)) return message.channel.send(`${m.author}, you can only bid ${config.maxbid} ${config.currency} above the current price.`);
+        else if ((bid % 100) != 0) return message.channel.send(`${m.author}, your bid must be divisible by 100.`);
+
+        clearTimeout(bidtimer);
+        m.channel.setTopic(`Player on sale: ${target.name} | Current bid: ${bid} ${config.currency} by ${bidder.name}`);
+        price = bid;
+
+        console.log(`${bidder.name} bid ${bid} ${config.currency}`);
+
+        // make a new timer
+        bidtimer = setTimeout(function ()
+        {
+            collector.stop();
+
+            // put player to sold
+            var soldObj = { name: target.name, owner: bidder.name, price: price };
+            stat.sold.push(soldObj);
+
+            // mark player as sold
+            for (var i = 0; i < stat.players.length; i++)
+            {
+                if (stat.players[i].name == target.name)
+                {
+                    stat.players[i].sold = " + "
+                    break;
+                }
+            }
+
+            // add sold player to captain and subtract money
+            for (var i = 0; i < stat.captains.length; i++)
+            {
+                if (stat.captains[i].name == bidder.name)
+                {
+                    stat.captains[i].slaves.push(target);
+                    stat.captains[i].money -= price;
+                    break;
+                }
+            }
+
+            message.channel.send(`${target.name} was sold to ${m.author} for ${price} ${config.currency}!`)
+
+            fs.writeFile("stats.json", JSON.stringify(stat), function (err)
+            {
+                if (err) return console.log("error", err);
+                return console.log("Save successful.");
+            });
+
+            biddingActive = false;
+
+            return message.channel.setTopic("");
+
+        }, config.timer)
+    });
+
+    collector.on('end', collected =>
+    {
+        console.log(`Collected ${collected.size} items`);
+    });
 
     return;
+}
+
+module.exports.help = {
+    name: "bid.start"
 }
