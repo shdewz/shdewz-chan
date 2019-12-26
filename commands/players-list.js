6fff1119a5d9c6326da550fa81adf02461c39f62
shdewz <shdewzofficial@gmail.com>
2019-12-26 23:11:30 +0200

    Display stats for sold players

--- commands/players-list.js
+++ commands/players-list.js
@@ -1,49 +1,67 @@
 const Discord = require('discord.js');
+const config = require("../config.json");
 
-module.exports = {
-    name: 'players.list',
-    description: 'Lists players.',
-    execute(message, args, stat)
+module.exports.run = async (client, message, args) =>
+{
+    var stat = client.commands.get("loadstats").run(); // load stats
+
+    if (args.length >= 1) // single player
     {
-        if (args.length >= 1) // single player
+        for (var i = 0; i < stat.players.length; i++)
         {
-            for (var i = 0; i < stat.players.length; i++)
+            if (stat.players[i].name.toLowerCase() == args[0].toLowerCase())
             {
-                if (stat.players[i].name.toLowerCase() == args[0].toLowerCase())
+                // player is sold
+                if (stat.players[i].sold != "   ")
                 {
-                    message.channel.send(`**Player name:** \`${stat.players[i].name}\`\n**Pickup line:**    ${stat.players[i].story}\s**Sold?** ${stat.players[i].sold}`);
-                    return;
+                    for (var j = 0; j < stat.sold.length; j++)
+                    {
+                        if (stat.sold[j].name == stat.players[i].name)
+                        {
+                            var owner = stat.sold[j].owner;
+                            var price = stat.sold[j].price;
+                        }
+                    }
+                    return message.channel.send(`**Player name:** \`${stat.players[i].name}\`\n**Pickup line:** ${stat.players[i].story}\n**Sold?** Yes, to ${owner} for ${price} ${config.currency}`);
                 }
-            }
-        }
-        else
-        {
-            if (stat.players.length == 0) return message.channel.send(`Player list is probably empty.`);
-            else
-            {
-                var plrListTextNames = "";
-                var plrListTextSold = "";
-
-                for (var i = 0; i < stat.players.length; i++)
+                else
                 {
-                    plrListTextNames += `\`${stat.players[i].name}\`\n`;
-                    plrListTextSold += `\`${stat.players[i].sold}\`\n`;
+                    return message.channel.send(`**Player name:** \`${stat.players[i].name}\`\n**Pickup line:** ${stat.players[i].story}\n**Sold?** No`);
                 }
-
-                if (plrListTextNames == "")
-                {
-                    message.channel.send(`Player list is probably empty.`);
-                    return;
-                }
-
-                const playerListEmbed = new Discord.RichEmbed()
-                    .setColor('#ff007a')
-                    .setTitle(`**Current players** (${stat.players.length})`)
-                    .addField('*Name*', plrListTextNames, true)
-                    .addField('*Sold?*', plrListTextSold, true)
-                message.channel.send(playerListEmbed);
-                return;
             }
         }
     }
+    else
+    {
+        if (stat.players.length == 0) return message.channel.send(`Player list is probably empty.`);
+        else
+        {
+            var plrListTextNames = "";
+            var plrListTextSold = "";
+
+            for (var i = 0; i < stat.players.length; i++)
+            {
+                plrListTextNames += `\`${stat.players[i].name}\`\n`;
+                plrListTextSold += `\`${stat.players[i].sold}\`\n`;
+            }
+
+            if (plrListTextNames == "")
+            {
+                message.channel.send(`Player list is probably empty.`);
+                return;
+            }
+
+            const playerListEmbed = new Discord.RichEmbed()
+                .setColor('#ff007a')
+                .setTitle(`**Current players** (${stat.players.length})`)
+                .addField('*Name*', plrListTextNames, true)
+                .addField('*Sold?*', plrListTextSold, true)
+            message.channel.send(playerListEmbed);
+            return;
+        }
+    }
 };
+
+module.exports.help = {
+    name: "players.list"
+}
