c5688fa20d55b71bfa62667c525dbd001d833e76
shdewz <shdewzofficial@gmail.com>
2019-12-26 23:11:13 +0200

    Clear sold status

--- commands/players-set-unsold.js
+++ commands/players-set-unsold.js
@@ -1,94 +1,105 @@
 const fs = require("fs");
 const config = require("../config.json");
 
-module.exports = {
-    name: 'players.set-unsold',
-    description: 'Sets player(s) as unsold.',
-    execute(message, args, stat)
+module.exports.run = async (client, message, args) =>
+{
+    if (!message.member.hasPermission("ADMINISTRATOR")) { message.reply(`Insufficient permissions`); return; }
+
+    var stat = client.commands.get("loadstats").run(); // load stats
+
+    if (args.length == 0)
     {
-        if (!message.member.hasPermission("ADMINISTRATOR")) { message.reply(`Insufficient permissions`); return; }
-        if (args.length == 0)
+        stat.unsold = stat.players;
+        stat.sold = [];
+
+        // clear sold status
+        for (var i = 0; i < stat.players.length; i++)
         {
-            stat.unsold = stat.players;
-            stat.sold = [];
-            // clear captains slaves and money
-            for (var i = 0; i < stat.captains.length; i++)
-            {
-                stat.captains[i].money = config.startmoney;
-                stat.captains[i].slaves = [];
-            }
-            fs.writeFile("stats.json", JSON.stringify(stat), function (err)
-            {
-                if (err) console.log("error", err);
-                message.channel.send(`Succesfully set all players as unsold.`);
-                return;
-            });
+            stat.players[i].sold = "   ";
         }
-        else
+
+        // clear captains slaves and money
+        for (var i = 0; i < stat.captains.length; i++)
         {
-            var plrListText = "";
-            var plrListTextMissing = "";
+            stat.captains[i].money = config.startmoney;
+            stat.captains[i].slaves = [];
+        }
 
-            for (var i = 0; i < args.length; i++)
+        fs.writeFile("stats.json", JSON.stringify(stat), function (err)
+        {
+            if (err) console.log("error", err);
+            message.channel.send(`Succesfully set all players as unsold.`);
+            return;
+        });
+    }
+    else
+    {
+        var plrListText = "";
+        var plrListTextMissing = "";
+
+        for (var i = 0; i < args.length; i++)
+        {
+            mainLoop:
+            for (var j = 0; j < stat.players.length; j++)
             {
-                mainLoop:
-                for (var j = 0; j < stat.players.length; j++)
+                if (stat.players[j].name == args[i])
                 {
-                    if (stat.players[j].name == args[i])
+                    var obj = { "name": stat.players[j].name, "story": stat.players[j].story };
+                    stat.unsold.push(obj);
+                    plrListText += `\`${args[i]}\`, `;
+
+                    // remove from sold if exists
+                    for (var k = 0; k < stat.sold.length; k++)
                     {
-                        var obj = { "name": stat.players[j].name, "story": stat.players[j].story };
-                        stat.unsold.push(obj);
-                        plrListText += `\`${args[i]}\`, `;
-
-                        // remove from sold if exists
-                        for (var k = 0; k < stat.sold.length; k++)
+                        if (stat.sold[k].name == args[i])
                         {
-                            if (stat.sold[k].name == args[i])
+                            // remove from captains
+                            for (var l = 0; l < stat.captains.length; l++)
                             {
-                                // remove from captains
-                                for (var l = 0; l < stat.captains.length; l++)
+                                if (stat.captains[l].name == stat.sold[k].owner)
                                 {
-                                    if (stat.captains[l].name == stat.sold[k].owner)
+                                    for (var m = 0; m < stat.captains[l].slaves.length; m++)
                                     {
-                                        for (var m = 0; m < stat.captains[l].slaves.length; m++)
+                                        if (stat.captains[l].slaves[m].name == args[i])
                                         {
-                                            if (stat.captains[l].slaves[m].name == args[i])
-                                            {
-                                                stat.captains[l].slaves.splice(m, 1);
+                                            stat.captains[l].slaves.splice(m, 1);
 
-                                                stat.sold.splice(k, 1);
-                                                break mainLoop;
-                                            }
+                                            stat.sold.splice(k, 1);
+                                            break mainLoop;
                                         }
                                     }
                                 }
                             }
                         }
-                        stat.sold.splice(k, 1);
-                        break mainLoop;
                     }
+                    stat.sold.splice(k, 1);
+                    break mainLoop;
                 }
             }
-
-            plrListText = plrListText.substring(0, plrListText.length - 2);
-            plrListTextMissing = plrListTextMissing.substring(0, plrListTextMissing.length - 2);
-
-            fs.writeFile("stats.json", JSON.stringify(stat), function (err)
-            {
-                if (err) console.log("error", err);
-
-                if (plrListTextMissing != "")
-                {
-                    message.channel.send(`${plrListTextMissing} not recognized as a player.`);
-                }
-                if (plrListText != "")
-                {
-                    message.channel.send(`Succesfully set ${plrListText} as unsold.`);
-                }
-                console.log(stat);
-                return;
-            });
         }
-        return;
+
+        plrListText = plrListText.substring(0, plrListText.length - 2);
+        plrListTextMissing = plrListTextMissing.substring(0, plrListTextMissing.length - 2);
+
+        fs.writeFile("stats.json", JSON.stringify(stat), function (err)
+        {
+            if (err) console.log("error", err);
+
+            if (plrListTextMissing != "")
+            {
+                message.channel.send(`${plrListTextMissing} not recognized as a player.`);
+            }
+            if (plrListText != "")
+            {
+                message.channel.send(`Succesfully set ${plrListText} as unsold.`);
+            }
+            console.log(stat);
+            return;
+        });
     }
+    return;
 };
+
+module.exports.help = {
+    name: "players.set-unsold"
+}
