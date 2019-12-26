4946c89bd1858216f062214f91e6b91a82f2a3f1
shdewz <shdewzofficial@gmail.com>
2019-12-26 23:11:00 +0200

    Make asynchronous

--- commands/loadstats.js
+++ commands/loadstats.js
@@ -1,43 +1,43 @@
 const fs = require("fs");
 
-module.exports = {
-    name: 'loadstats',
-    description: 'Refreshes/Loads stats.json.',
-    execute()
+module.exports.run = function ()
+{
+    try
     {
-        try
+        if (fs.existsSync("stats.json"))
         {
-            if (fs.existsSync("stats.json"))
-            {
-                var stat = JSON.parse(fs.readFileSync("stats.json", "utf-8"));
-                return stat;
-            }
-            else
-            {
-                stat = {
-                    "captains": [
-                    ],
-                    "sold": [
-                    ],
-                    "unsold": [
-                    ],
-                    "players": [
-                    ]
-                };
-
-                fs.writeFileSync("stats.json", JSON.stringify(stat), function (err)
-                {
-                    if (err) console.log("error", err);
-
-                    console.log("Stats created succesfully\n" + stat + "\n\n")
-                    return stat;
-                });
-            }
+            var stat = JSON.parse(fs.readFileSync("stats.json", "utf-8"));
+            return stat;
         }
-        catch (error)
+        else
         {
-            console.log(error);
-            return;
+            stat = {
+                "captains": [
+                ],
+                "sold": [
+                ],
+                "unsold": [
+                ],
+                "players": [
+                ]
+            };
+
+            fs.writeFileSync("stats.json", JSON.stringify(stat), function (err)
+            {
+                if (err) console.log("error", err);
+
+                console.log("Stats created succesfully\n" + stat + "\n\n")
+                return stat;
+            });
         }
     }
+    catch (error)
+    {
+        console.log(error);
+        return;
+    }
 };
+
+module.exports.help = {
+    name: "loadstats"
+}
--- commands/players-clear.js
+++ commands/players-clear.js
@@ -1,28 +1,30 @@
 const fs = require("fs");
 
-module.exports = {
-    name: 'players.clear',
-    description: 'Clears all players.',
-    execute(message, args, stat)
+module.exports.run = async (client, message, args) =>
+{
+    if (!message.member.hasPermission("ADMINISTRATOR")) { message.reply(`Insufficient permissions`); return; }
+
+    var stat = client.commands.get("loadstats").run(); // load stats
+
+    stat.players = [];
+    stat.sold = [];
+    stat.unsold = [];
+    for (var i = 0; i < stat.captains.length; i++)
     {
-        if (!message.member.hasPermission("ADMINISTRATOR")) { message.reply(`Insufficient permissions`); return; }
-
-        stat.players = [];
-        stat.sold = [];
-        stat.unsold = [];
-        for (var i = 0; i < stat.captains.length; i++)
-        {
-            stat.captains[i].slaves = [];
-        }
-
-        fs.writeFile("stats.json", JSON.stringify(stat), function (err)
-        {
-            if (err) console.log("error", err);
-
-            message.channel.send(`Succesfully cleared all players.`);
-            console.log(stat.players);
-            return;
-        });
-        return;
+        stat.captains[i].slaves = [];
     }
+
+    fs.writeFile("stats.json", JSON.stringify(stat), function (err)
+    {
+        if (err) console.log("error", err);
+
+        message.channel.send(`Succesfully cleared all players.`);
+        console.log(stat.players);
+        return;
+    });
+    return;
 };
+
+module.exports.help = {
+    name: "players.clear"
+}
