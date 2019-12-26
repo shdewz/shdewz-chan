4fb75277b6a29c0799468cd1b5b2ac26d82093a7
shdewz <shdewzofficial@gmail.com>
2019-12-26 23:10:36 +0200

    Make asynchronous

--- commands/captains-list.js
+++ commands/captains-list.js
@@ -1,61 +1,57 @@
 const Discord = require('discord.js');
 const config = require("../config.json");
 
-module.exports = {
-    name: 'captains.list',
-    description: 'Lists captains.',
-    execute(message, args, stat)
+module.exports.run = async (client, message, args) =>
+{
+    var stat = client.commands.get("loadstats").run(); // load stats
+
+    var cptListTextNames = "";
+    var cptListTextMoney = "";
+    var cptListTextSlaves = "";
+
+    if (args.length >= 1) // single stats
     {
-        var cptListTextNames = "";
-        var cptListTextMoney = "";
-        var cptListTextSlaves = "";
-
-        if (args.length >= 1) // single stats
+        for (var i = 0; i < stat.captains.length; i++)
         {
-            for (var i = 0; i < stat.captains.length; i++)
+            if (stat.captains[i].name.toLowerCase() == args[0].toLowerCase())
             {
-                if (stat.captains[i].name.toLowerCase() == args[0].toLowerCase())
+                for (var j = 0; j < stat.captains[i].slaves.length; j++)
                 {
-                    for (var j = 0; j < stat.captains[i].slaves.length; j++)
-                    {
-                        cptListTextSlaves += `\`${stat.captains[i].slaves[j].name}\`, `;
-                    }
-                    cptListTextSlaves = cptListTextSlaves.substring(0, cptListTextSlaves.length - 2);
-                    message.channel.send(`**Captain:** \`${stat.captains[i].name}\`\n**Funds:** \`${ac(stat.captains[i].money)} ${config.currency}\`\n**Slaves:** ${cptListTextSlaves} (${stat.captains[i].slaves.length})`);
-                    return;
+                    cptListTextSlaves += `\`${stat.captains[i].slaves[j].name}\`, `;
                 }
-            }
-        }
-        else // overall stats
-        {
-            for (var i = 0; i < stat.captains.length; i++)
-            {
-                cptListTextNames += `\`${stat.captains[i].name}\`\n`;
-                cptListTextMoney += `\`${ac(stat.captains[i].money)}\`\n`;
-                cptListTextSlaves += `\`${stat.captains[i].slaves.length}\`\n`;
-            }
-
-            if (cptListTextNames == "")
-            {
-                message.channel.send(`Captains list is probably empty.`);
+                cptListTextSlaves = cptListTextSlaves.substring(0, cptListTextSlaves.length - 2);
+                message.channel.send(`**Captain:** \`${stat.captains[i].name}\`\n**Funds:** \`${stat.captains[i].money.toLocaleString()} ${config.currency}\`\n**Slaves:** ${cptListTextSlaves} (${stat.captains[i].slaves.length})`);
                 return;
             }
+        }
+    }
+    else // overall stats
+    {
+        for (var i = 0; i < stat.captains.length; i++)
+        {
+            cptListTextNames += `\`${stat.captains[i].name}\`\n`;
+            cptListTextMoney += `\`${stat.captains[i].money.toLocaleString()}\`\n`;
+            cptListTextSlaves += `\`${stat.captains[i].slaves.length}\`\n`;
+        }
 
-            const captainListEmbed = new Discord.RichEmbed()
-                .setColor('#ff007a')
-                .setTitle(`**Current captains** (${stat.captains.length})`)
-                .addField('*Name*', cptListTextNames, true)
-                .addField(`*${config.currency}*`, cptListTextMoney, true)
-                .addField('*Slaves*', cptListTextSlaves, true)
-            message.channel.send(captainListEmbed);
-
+        if (cptListTextNames == "")
+        {
+            message.channel.send(`Captains list is probably empty.`);
             return;
         }
+
+        const captainListEmbed = new Discord.RichEmbed()
+            .setColor('#ff007a')
+            .setTitle(`**Current captains** (${stat.captains.length})`)
+            .addField('*Name*', cptListTextNames, true)
+            .addField(`*${config.currency}*`, cptListTextMoney, true)
+            .addField('*Slaves*', cptListTextSlaves, true)
+        message.channel.send(captainListEmbed);
+
+        return;
     }
 };
 
-// add comma as thousand separator
-function ac(num)
-{
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+module.exports.help = {
+    name: "captains.list"
 }
