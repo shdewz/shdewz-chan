7445c0841032ba7cb9861142de70a109eec4ea95
shdewz <shdewzofficial@gmail.com>
2020-01-16 20:16:41 +0200

    Display mp link

--- commands/players-list.js
+++ commands/players-list.js
@@ -1,41 +1,35 @@
 const Discord = require('discord.js');
 const config = require("../config.json");
 
-module.exports.run = async (client, message, args) =>
-{
+module.exports.run = async (client, message, args) => {
     var stat = client.commands.get("loadstats").run(); // load stats
+    var mplink;
 
     if (args.length >= 1 && !args.includes("-p")) // single player
     {
-        for (var i = 0; i < stat.players.length; i++)
-        {
-            if (stat.players[i].name.toLowerCase() == args[0].toLowerCase())
-            {
+        for (var i = 0; i < stat.players.length; i++) {
+            if (stat.players[i].name.toLowerCase() == args[0].toLowerCase()) {
+                if (typeof stat.players[i].mp !== "undefined" && stat.players[i].mp != "") mplink = `<${stat.players[i].mp}>`;
+                else mplink = "N/A";
                 // player is sold
-                if (stat.players[i].sold != " - ")
-                {
-                    for (var j = 0; j < stat.sold.length; j++)
-                    {
-                        if (stat.sold[j].name == stat.players[i].name)
-                        {
+                if (stat.players[i].sold != " - ") {
+                    for (var j = 0; j < stat.sold.length; j++) {
+                        if (stat.sold[j].name == stat.players[i].name) {
                             var owner = stat.sold[j].owner;
                             var price = stat.sold[j].price;
                         }
                     }
-                    return message.channel.send(`**Player name:** ${stat.players[i].name.replace(/([_*~])/g, "\\$1")}\n**Pickup line:** ${stat.players[i].story}\n**Sold?** Yes, to ${owner} for ${price} ${config.currency}`);
+                    return message.channel.send(`**Player name:** ${stat.players[i].name.replace(/([_*~])/g, "\\$1")}\n**Pickup line:** ${stat.players[i].story}\n**MP Link:** ${mplink}\n**Sold?** Yes, to ${owner} for ${price} ${config.currency}`);
                 }
-                else
-                {
-                    return message.channel.send(`**Player name:** ${stat.players[i].name.replace(/([_*~])/g, "\\$1")}\n**Pickup line:** ${stat.players[i].story}\n**Sold?** No`);
+                else {
+                    return message.channel.send(`**Player name:** ${stat.players[i].name.replace(/([_*~])/g, "\\$1")}\n**Pickup line:** ${stat.players[i].story}\n**MP Link:** ${mplink}\n**Sold?** No`);
                 }
             }
         }
     }
-    else
-    {
+    else {
         if (stat.players.length == 0) return message.channel.send(`Player list is probably empty.`);
-        else
-        {
+        else {
             var plrListTextNames = "";
             var plrListTextSold = "";
             var plrListMPLinks = "";
@@ -45,15 +39,13 @@ module.exports.run = async (client, message, args) =>
             var pages = Math.ceil(stat.players.length / 50)
             var page;
 
-            if (args.includes("-p") && !isNaN(args[args.length - 1]))
-            {
+            if (args.includes("-p") && !isNaN(args[args.length - 1])) {
                 page = args[args.length - 1];
                 if (args[args.length - 1] > pages) return message.channel.send(`Not enough pages.`);
                 pageStart = 50 * (page - 1);
                 pageEnd = 50 + (50 * (page - 1));
             }
-            else
-            {
+            else {
                 page = 1;
                 pageStart = 0;
                 pageEnd = 50;
@@ -61,16 +53,14 @@ module.exports.run = async (client, message, args) =>
 
             if (pageEnd > stat.players.length) pageEnd = stat.players.length;
 
-            for (var i = pageStart; i < pageEnd; i++)
-            {
+            for (var i = pageStart; i < pageEnd; i++) {
                 plrListTextNames += `${stat.players[i].name.replace(/([_*~])/g, "\\$1")}\n`;
                 plrListTextSold += `${stat.players[i].sold}\n`;
                 if (stat.players[i].mp && stat.players[i].mp != "") plrListMPLinks += `[mp link](${stat.players[i].mp})\n`;
                 else plrListMPLinks += `-\n`;
             }
 
-            if (plrListTextNames == "")
-            {
+            if (plrListTextNames == "") {
                 message.channel.send(`Player list is probably empty.`);
                 return;
             }
