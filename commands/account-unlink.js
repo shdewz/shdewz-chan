8536493b74e5f45f134ff35989d78b260ed60eb8
shdewz <shdewzofficial@gmail.com>
2020-01-16 20:15:57 +0200

    Remove second writefile

--- commands/account-unlink.js
+++ commands/account-unlink.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
-module.exports.run = async (client, message, args) =>
-{
+module.exports.run = async (client, message, args) => {
     if (args.length > 0) var id = args[0];
     else var id = message.author.id;
 
@@ -10,44 +9,30 @@ module.exports.run = async (client, message, args) =>
     var stat = client.commands.get("loadstats").run(); // load stats
 
     // see if captain
-    captain:
-    for (var i = 0; i < stat.captains.length; i++)
-    {
-        if (stat.captains[i].dc == id)
-        {
+    for (var i = 0; i < stat.captains.length; i++) {
+        if (stat.captains[i].dc == id) {
             stat.captains[i].dc = "";
-
-            fs.writeFile("stats.json", JSON.stringify(stat), function (err)
-            {
-                if (err) return console.log("error", err);
-                return console.log("Save successful.");
-            });
             found = true;
-            message.reply(`succesfully unlinked from ${stat.captains[i].name}.`)
-            break captain;
+            message.reply(`succesfully unlinked from captain ${stat.captains[i].name}.`)
         }
     }
 
     // see if player
-    player:
     for (var i = 0; i < stat.players.length; i++) // go through the list
     {
-        if (stat.players[i].dc == id) // see if name exists in players list
-        {
+        if (stat.players[i].dc == id) {
             stat.players[i].dc = "";
-
-            fs.writeFile("stats.json", JSON.stringify(stat), function (err)
-            {
-                if (err) return console.log("error", err);
-                return console.log("Save successful.");
-            });
             found = true;
-            message.reply(`succesfully unlinked from ${stat.players[i].name}.`)
-            break player;
+            message.reply(`succesfully unlinked from player ${stat.players[i].name}.`)
         }
     }
 
-    if (!found) message.reply(`${name} nothing to unlink.`);
+    fs.writeFile("stats.json", JSON.stringify(stat), function (err) {
+        if (err) return console.log("error", err);
+        return console.log("Save successful.");
+    });
+
+    if (!found) message.reply(`nothing to unlink.`);
     return;
 }
 
