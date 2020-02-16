bea7ffd238127d97140a450152ef75a2233e56dc
shdewz <shdewzofficial@gmail.com>
2020-02-16 02:57:35 +0200

    Initial

---
+++ commands/help.js
@@ -0,0 +1,85 @@
+const config = require("../config.json");
+
+module.exports.run = async (message, args, client) => {
+
+    if (args[0]) {
+        var command = args[0];
+        if (client.commands.has(command) || client.aliases.has(command)) {
+            if (client.commands.has(command)) command = client.commands.get(command);
+            else command = client.commands.get(client.aliases.get(command));
+
+            var exampletext = "";
+            if (command.help.example) exampletext = `\n**Example:** \`${config.prefix}${command.help.example}\``;
+
+            if (!command.help.usage) var usage = command.help.name;
+            else var usage = command.help.usage;
+
+            let embed = {
+                color: 0xe84393,
+                author: {
+                    name: `${config.prefix}${command.help.name}`
+                },
+                description: `*${command.help.description}*\n\n**Usage:** \`${config.prefix}${usage}\`${exampletext}`,
+                footer: {
+                    text: `Use ${config.prefix}help for a list of all commands`
+                }
+            }
+            return message.channel.send({ embed: embed });
+        }
+        else return message.reply(`command ${command} not found.`);
+    }
+    else {
+        var commands = [];
+        for (var i = 0; i < Array.from(client.commands).length; i++) {
+            var command = Array.from(client.commands)[i][1];
+            if (!command.help) continue;
+            if (command.help && command.help.category == "sys") continue;
+
+            var found = false;
+            for (var n = 0; n < commands.length; n++) {
+                if (commands[n].category && commands[n].category == command.help.category) {
+                    if (commands[n].names) {
+                        commands[n].names += `\`${command.help.name}\` `;
+                    }
+                    else commands[n].names = `\`${command.help.name}\` `;
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                var obj = { category: command.help.category, names: `\`${command.help.name}\` ` };
+                commands.push(obj);
+            }
+        }
+
+        var fields = [];
+        for (var i = 0; i < commands.length; i++) {
+            var obj = { name: commands[i].category, value: commands[i].names };
+            fields.push(obj);
+        }
+
+        let embed = {
+            color: 0xe84393,
+            author: {
+                name: `shdewz-chan command list`,
+                icon_url: `https://i.imgur.com/VUeXUCY.jpg`
+            },
+            fields: fields,
+            footer: {
+                text: `use '${config.prefix}help <command>' for detailed info about a certain command`
+            }
+        }
+        return message.channel.send({ embed: embed });
+
+        return console.log(commands); // list all commands
+    }
+};
+
+module.exports.help = {
+    name: "help",
+    aliases: ["commands"],
+    description: "Shows help about different commands",
+    usage: "help <command>",
+    example: "help osu",
+    category: "General"
+}
