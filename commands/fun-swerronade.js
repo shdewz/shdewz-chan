e050490a6cfc04eb1e8389f140c282b791ef87b8
shdewz <shdewzofficial@gmail.com>
2020-02-28 15:30:06 +0200

    Initial

---
+++ commands/fun-swerronade.js
@@ -0,0 +1,9 @@
+module.exports.run = async (message) => {
+    return message.channel.send("https://twitter.com/Mahloola/status/1230616320334585856");
+};
+
+module.exports.help = {
+    name: "swerronade",
+    description: "SWERRO NADE",
+    category: "Fun"
+}
