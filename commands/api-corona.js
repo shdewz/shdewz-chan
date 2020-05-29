00d324ed82060a4fbf738d2a9c909e60328760b4
shdewz <shdewzofficial@gmail.com>
2020-05-30 02:21:50 +0300

    Simplify function

--- commands/api-corona.js
+++ commands/api-corona.js
@@ -345,11 +345,8 @@ module.exports.run = async (message, args) => {
 
 function GetSortOrder(prop) {
     return function (a, b) {
-        if (a[prop] > b[prop]) {
-            return 1;
-        } else if (a[prop] < b[prop]) {
-            return -1;
-        }
+        if (a[prop] > b[prop]) return 1;
+        else if (a[prop] < b[prop]) return -1;
         return 0;
     }
 }
