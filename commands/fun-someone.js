2203318a9c3c7c94885fce7329714137b3f7bec3
shdewz <shdewzofficial@gmail.com>
2020-09-07 13:48:44 +0300

    Add role filter

--- commands/fun-someone.js
+++ commands/fun-someone.js
@@ -2,6 +2,8 @@ module.exports.run = async (message, args, client) => {
     var ids = [];
     var members;
     var ping = false;
+    let csrole;
+    if (message.guild.id == "265497141013643276") csrole = message.member.roles.cache.get("275033417907699713");
 
     if (args.includes("-ping")) {
         ping = true;
@@ -9,13 +11,25 @@ module.exports.run = async (message, args, client) => {
     }
 
     if (args.includes("-online")) {
-        members = Array.from(message.guild.members.filter(member => member.presence.status !== "offline" && !member.user.bot));
+        if (args.includes("-cs") && message.guild.id == "265497141013643276") {
+            members = Array.from(message.guild.members.cache.filter(member => member.presence.status !== "offline" && !member.user.bot && member.roles.has(csrole)));
+            args.splice(args.indexOf("-cs"), 1);
+        }
+        else members = Array.from(message.guild.members.cache.filter(member => member.presence.status !== "offline" && !member.user.bot));
         args.splice(args.indexOf("-online"), 1);
-    } else members = Array.from(message.guild.members.filter(member => !member.user.bot));
+    } else {
+        if (args.includes("-cs") && message.guild.id == "265497141013643276") {
+            members = Array.from(message.guild.members.cache.filter(member => !member.user.bot && member.roles.has(csrole)));
+            args.splice(args.indexOf("-cs"), 1);
+        }
+        else {
+            members = Array.from(message.guild.members.cache.filter(member => !member.user.bot));
+        }
+    }
 
     members.forEach(member => ids.push(member[0]));
 
-    var user = client.users.get(ids[Math.floor(Math.random() * ids.length)]);
+    var user = client.users.cache.get(ids[Math.floor(Math.random() * ids.length)]);
 
     message.channel.send(`${ping ? user : message.guild.member(user).displayName} ${args.join(" ")}`);
 }
