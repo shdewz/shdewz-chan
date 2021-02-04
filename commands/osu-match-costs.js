abc69f22ec292f053fe9dd6ca8c27dab27c7edbc
shdewz <shdewzofficial@gmail.com>
2021-02-04 02:55:44 +0200

    add comments in case someone wants to read this

--- commands/osu-match-costs.js
+++ commands/osu-match-costs.js
@@ -8,10 +8,11 @@ module.exports.run = async (message, args, client) => {
     args = args.map(a => a.toLowerCase());
     let match_id = args[0].match(/\d+/)[0];
 
+    // get the match data from api
     let s = await osu.getMatch(match_id);
     if (s.error) return message.channel.send(s.error);
 
-
+    // remove maps from start and end
     if (args[1] && !isNaN(args[1])) s.games.splice(0, args[1]);
     if (args[2] && !isNaN(args[2])) s.games.splice(-args[2], args[2]);
 
@@ -29,9 +30,11 @@ module.exports.run = async (message, args, client) => {
         medianmaps: 0
     };
 
+    // get a list of players from the lobby
     let unique_players = s.games.map(g => g.scores.filter(s => s.score > 0).map(s => s.user_id)).reduce((a, b) => a.concat(b), []).filter((p, i, a) => a.indexOf(p) == i);
 
     return new Promise(resolve => {
+        // make a pretty object for all of them
         unique_players.forEach(async player => {
             let user = await osu.getUser(player);
 
@@ -47,7 +50,7 @@ module.exports.run = async (message, args, client) => {
             if (match.players.length == unique_players.length) resolve();
         });
     }).then(() => {
-        // swap players if order wrong
+        // swap players in h2h if order wrong
         if (match.players.length == 2) {
             if (match.players[0].username !== match.team1 && match.players[1].username !== match.team2) {
                 [match.players[0], match.players[1]] = [match.players[1], match.players[0]];
@@ -55,21 +58,24 @@ module.exports.run = async (message, args, client) => {
         }
 
         s.games.filter(game => game.scores.length > 0).forEach(map => {
+            // add median score of map to median array
             match.medians.push(median(map.scores.map(sc => Number(sc.score))));
 
-            if (mode == 2) {
+            // increment points of winning team
+            if (mode == 2) { // teamvs
                 let team1score = map.scores.filter(sc => sc.team == 1).map(sc => Number(sc.score)).reduce((a, b) => a + b);
                 let team2score = map.scores.filter(sc => sc.team == 2).map(sc => Number(sc.score)).reduce((a, b) => a + b);
                 if (team1score > team2score) match.team1points++;
                 else match.team2points++;
             }
-            else if (match.players.length == 2) {
+            else if (match.players.length == 2) { // h2h
                 let team1score = map.scores.filter(sc => sc.user_id == match.players[0].user_id).map(sc => Number(sc.score))[0];
                 let team2score = map.scores.filter(sc => sc.user_id == match.players[1].user_id).map(sc => Number(sc.score))[0];
                 if (team1score > team2score) match.team1points++;
                 else match.team2points++;
             }
 
+            // add player score (and team if teamvs)
             match.players.forEach(player => {
                 let score = map.scores.find(s => s.user_id == player.user_id);
                 if (!score) player.scores.push(0);
@@ -80,13 +86,15 @@ module.exports.run = async (message, args, client) => {
             });
         });
 
+        // get the median amount of maps played per player
         let arr = [];
         match.players.forEach(player => arr.push(player.scores.filter(score => score > 0).length));
         match.medianmaps = median(arr);
 
+        // run the cost function for each player
         match.players.forEach(player => { player.rating = getCost(player.scores, match.medians, match.medianmaps) });
-        match.players.sort((a, b) => a.rating - b.rating).reverse();
-        for (var i = 0; i < Math.min(match.players.length, 3); i++) { match.players[i].medal = medals[i]; }
+        match.players.sort((a, b) => a.rating - b.rating).reverse(); // sort by rating
+        for (var i = 0; i < Math.min(match.players.length, 3); i++) { match.players[i].medal = medals[i]; } // add medals for top3
 
         let embed = {};
 
@@ -130,16 +138,11 @@ module.exports.run = async (message, args, client) => {
             }
         }
 
-        /*
-        console.log({ "medians": match.medians });
-        console.log(match.players.map(p => ({ "name": p.username, "scores": p.scores })));*/
         return message.channel.send({ embed: embed });
     });
 };
 
-const getCost = (p, m, mm) => { let sum = 0; p.forEach((s, i) => { if (s > 0) sum += s / m[i] }); return (sum / p.filter(s => s > 0).length) * Math.cbrt(p.filter(s => s > 0).length / mm); };
-
-const getCostAlt = (p, m) => { let sum = 0; p.forEach((s, i) => { if (s > 0) sum += s / m[i] }); return sum; };
+const getCost = (playerScores, medians, mapsPlayed) => { let sum = 0; playerScores.forEach((s, i) => { if (s > 0) sum += s / medians[i] }); return (sum / playerScores.filter(s => s > 0).length) * Math.cbrt(playerScores.filter(s => s > 0).length / mapsPlayed); };
 
 const median = arr => {
     const mid = Math.floor(arr.length / 2);
