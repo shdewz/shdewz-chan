dbd619571f000532a9502e0ff7ef841760ee46fb
shdewz <shdewz@gmail.com>
2024-11-08 15:11:18 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shdewz-chan
+
 A Discord bot with *osu!* -related tools and other cool more or less useful functions.
--- commands/fun-swerronade.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (message) => {
-    return message.channel.send("https://twitter.com/Mahloola/status/1230616320334585856");
-};
-
-module.exports.help = {
-    name: "swerronade",
-    description: "SWERRO NADE",
-    category: "Fun"
-}
