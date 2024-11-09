5bbb29f810c9fe73a27f114954b563d83adc36d3
shdewz <shdewz@gmail.com>
2024-11-09 23:28:38 +0200

    Clean up

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,12 +1,12 @@
 import { Client, Message } from 'discord.js';
-import { parseArgs } from '../../helpers/utils';
 
 const attributes = {
     name: 'ping',
-    aliases: []
+    aliases: [],
+    description: ''
 }
 
-export const execute = (client: Client, message: Message, _args: string[]) => {
+export const execute = (client: Client, message: Message, _args: string[], prefix: string) => {
     message.reply(`${(Date.now() - message.createdTimestamp).toLocaleString()}ms :ping_pong:`);
 };
 
