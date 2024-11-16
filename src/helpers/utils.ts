3653f27a401710a5a6c02c0759a3922d2414fbe3
shdewz <shdewz@gmail.com>
2024-11-16 13:59:57 +0200

    Add random number input

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -7,3 +7,13 @@ export const parseArgs = (args: string[]) => {
 
 export const formatNum = (num: number, format: string) => numeral(num).format(format);
 export const plural = (num: number) => num === 1 ? '' : 's';
+
+export const randomString = (str: string) => {
+    const match = str.match(/{r:(\d+)-(\d+)}/);
+    if (match) {
+        const diff = Number(match[2]) - Number(match[1]);
+        const num = Number(match[1]) + Math.floor(Math.random() * diff);
+        return str.replace(/{r:(\d+)-(\d+)}/, num.toString());
+    }
+    return str;
+}
