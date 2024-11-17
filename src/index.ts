96d9ff066d5c2e63312575916a858ddafeeeff85
shdewz <shdewz@gmail.com>
2024-11-17 14:12:48 +0200

    Separate dev and production tokens

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import { Client, Collection, GatewayIntentBits } from 'discord.js';
 
-import numeral from 'numeral';
-
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent],
     allowedMentions: { parse: ['users', 'roles'], repliedUser: true }
@@ -13,4 +11,4 @@ require('./helpers/db').init();
 require('./handlers/commandHandler').loadCommands(client);
 require('./handlers/eventHandler').loadEvents(client);
 
-client.login(process.env.TOKEN);
+client.login(process.env.ENV == 'production' ? process.env.TOKEN : process.env.TOKEN_DEV);
