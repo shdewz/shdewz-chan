e7820e0a5b2534f181d6b0d520f90653e56fada2
shdewz <shdewz@gmail.com>
2025-05-28 18:59:10 +0300

    Add score completion method

--- src/helpers/osu/utils.ts
+++ src/helpers/osu/utils.ts
@@ -1,7 +1,5 @@
 import { emotes, modes, modsEnum } from './constants.js';
 
-export const noAccountSet = '**You have not linked your osu! account yet!**\nDo it with the command `{{prefix}}set -osu <user>`';
-
 export const getEmote = (name: string) => { return emotes.find(r => r.name === name); };
 
 const parseMode = (searchString: string) => {
@@ -20,7 +18,7 @@ export const getMode = (modeArg: string, command: string) => {
 
 export const getDisplayMode = (mode: string) => modes.find(m => m.aliases.includes(mode))?.display;
 
-export const reverse_mods = (mods: string) => {
+export const reverseMods = (mods: string) => {
     if (!mods) return 0;
     let value = 0;
     const modsArray = mods.replace(/[^a-zA-z]/g, '').match(/.{2}/g);
@@ -32,4 +30,10 @@ export const reverse_mods = (mods: string) => {
 
 export const guessFC = (score: any, maxCombo: number) => {
     return score.max_combo > maxCombo * 0.998;
+};
+
+export const scoreCompletion = (hits: any, beatmap: any) => {
+    const userHits = hits.count_300 + hits.count_100 + hits.count_50 + hits.count_miss;
+    const maxHits = beatmap.count_circles + beatmap.count_sliders + beatmap.count_spinners;
+    return userHits / maxHits;
 };
